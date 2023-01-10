2a3ad4ee29c9c529159d2422c89feb7c7e9d93c9
limin <limin176868@gmail.com>
2023-01-10 13:21:58 +0800

    update 5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import x from "./x.js"
 import jpg from "./assets/1.jpg"
 
-// const div = document.getElementById("app")
+const div = document.getElementById("app")
 // div.innerHTML = `<img src="${jpg}">`
 
 
